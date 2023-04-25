9ee4cc1bc63d2425fe051a4f63fa314ff93f4b6f
tamandrew <atam2088@gmail.com>
2023-04-25 09:18:58 -0400

    Add hashcode() and equals() methods for Ids

--- backend/src/main/java/org/andrewtam/ChirpBoards/SQLModels/FollowId.java
+++ backend/src/main/java/org/andrewtam/ChirpBoards/SQLModels/FollowId.java
@@ -24,4 +24,21 @@ public class FollowId implements Serializable {
     public void setTarget(String target) {
         this.target = target;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (!(obj instanceof FollowId)) return false;
+
+        FollowId followId = (FollowId) obj;
+        return followId.user.equals(this.user) && followId.target.equals(this.target);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + user.hashCode();
+        result = 31 * result + target.hashCode();
+        return result;
+    }
 }
--- backend/src/main/java/org/andrewtam/ChirpBoards/SQLModels/VoteId.java
+++ backend/src/main/java/org/andrewtam/ChirpBoards/SQLModels/VoteId.java
@@ -24,4 +24,21 @@ public class VoteId implements Serializable {
     public void setUser(String user) {
         this.user = user;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (!(obj instanceof VoteId)) return false;
+
+        VoteId voteId = (VoteId) obj;
+        return voteId.post.equals(this.post) && voteId.user.equals(this.user);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + post.hashCode();
+        result = 31 * result + user.hashCode();
+        return result;
+    }
 }
